9f0ecb0cac8b89ede215a86e929d5dea108e016c
Tuomas Kassila <tuomas.kassila@gmail.com>
2021-12-26 17:09:44 +0200

    Corrected run.sh and applciation scene max height and width

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 # set ESPEAK_HOME="."
 # SET PATH=%ESPEAK_HOME%;%PATH%
 # echo %ESPEAK_HOME%
-java -cp ./lib\openpdf-1.3.26.jar:./lib\bcprov-ext-jdk15on-1.70.jar:./JavaFxPdfFooter.jar:@CLASSPATH com.metait.javafxpdffooter.JavaFxPdfFooterApplication
-rem com.metait.javafxplayer.testapp.HelloWorld
+export DISPLAY=:0
+echo java -cp ./lib/openpdf-1.3.26.jar:./lib/bcprov-ext-jdk15on-1.70.jar:./JavaFxPdfFooter.jar:$CLASSPATH com.metait.javafxpdffooter.JavaFxPdfFooterApplication
+java -cp ./lib/openpdf-1.3.26.jar:./lib/bcprov-ext-jdk15on-1.70.jar:./JavaFxPdfFooter.jar:$CLASSPATH com.metait.javafxpdffooter.JavaFxPdfFooterApplication
+# com.metait.javafxplayer.testapp.HelloWorld
--- src/main/java/com/metait/javafxpdffooter/JavaFxPdfFooterApplication.java
+++ src/main/java/com/metait/javafxpdffooter/JavaFxPdfFooterApplication.java
@@ -2,7 +2,9 @@ package com.metait.javafxpdffooter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +19,18 @@ public class JavaFxPdfFooterApplication extends Application {
         controller = new JavaFxFooterController();
         controller.setPrimaryStage(stage);
         fxmlLoader.setController(controller);
-        Scene scene = new Scene(fxmlLoader.load(), 904, 700);
+
+        double scene_with = 904;
+        double scene_height = 700;
+        //Get primary screen bounds
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        if (screenBounds.getHeight() < scene_height)
+            scene_height = screenBounds.getHeight();
+        if (screenBounds.getWidth() < scene_with)
+            scene_with = screenBounds.getWidth();
+        System.out.println(screenBounds);
+
+        Scene scene = new Scene(fxmlLoader.load(), scene_with, scene_height);
         stage.setTitle("JavaFxPdfFooter");
         stage.setScene(scene);
         stage.show();
